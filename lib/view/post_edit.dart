7f671bd77660a82bad903aa6e5af37be61a796ed
Amir55143 <aamirnoor55143@gmail.com>
2023-10-13 16:16:46 +0500

    Post-Edit

--- lib/view/post_edit.dart
+++ lib/view/post_edit.dart
@@ -1,10 +1,72 @@
+import 'package:api_crud/services/http_helper.dart';
 import 'package:flutter/material.dart';
 
-class PostEdit extends StatelessWidget {
-  const PostEdit({super.key});
+
+// ignore: must_be_immutable
+class PostEdit extends StatefulWidget {
+  PostEdit(this.post, {Key? key}) : super(key: key);
+  Map post;
+
+  @override
+  State<PostEdit> createState() => _PostEditState();
+}
+
+class _PostEditState extends State<PostEdit> {
+  final TextEditingController _controllerTitle = TextEditingController();
+  final TextEditingController _controllerBody = TextEditingController();
+
+  @override
+  initState() {
+    super.initState();
+    _controllerTitle.text = widget.post['title'];
+    _controllerBody.text = widget.post['body'];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Post'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controllerTitle,
+              ),
+              TextFormField(
+                controller: _controllerBody,
+                maxLines: 5,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    Map<String, String> dataToUpdate = {
+                      'title': _controllerTitle.text,
+                      'body': _controllerBody.text,
+                    };
+
+                    bool status = await HttpHelper()
+                        .updateItem(dataToUpdate, widget.post['id'].toString());
+
+                    if (status) {
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(const SnackBar(content: Text('Post updated')));
+                    }
+                    else
+                      {
+                        // ignore: use_build_context_synchronously
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(content: Text('Failed to update the post')));
+                      }
+                  },
+                  child: const Text('Submit'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
